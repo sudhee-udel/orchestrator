4c7355abb56243543ba7dc1d306652e43dc528b0
Sudheendra Ayalasomayajula <sudhee@Sudheendras-MacBook-Pro.local>
2019-05-23 06:43:48 -0400

    reading list of values for associated endpoints from json file in resources folder and only displaying relevant values for endpoint in browser

--- src/main/java/com/example/orchestrator/controllers/MyController.java
+++ src/main/java/com/example/orchestrator/controllers/MyController.java
@@ -1,16 +1,20 @@
 package com.example.orchestrator.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 @RestController
 public class MyController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private Map<String, List<Map<String, String>>> values;
 
@@ -22,7 +26,8 @@ public class MyController {
     }
 
     @RequestMapping("/myendpoint")
-    public Map showValues() {
-        return this.values;
+    public Object showValues(HttpServletRequest httpServletRequest) {
+        logger.info(httpServletRequest.getRequestURI());
+        return this.values.get(httpServletRequest.getRequestURI());
     }
 }
