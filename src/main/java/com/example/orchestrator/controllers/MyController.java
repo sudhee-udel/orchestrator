de87043d80eb83c2a392b7f604db97c0014d42bf
Sudheendra Ayalasomayajula <sudhee@Sudheendras-MacBook-Pro.local>
2019-05-23 06:14:29 -0400

    reading value from hardcoded json and displaying it in browser

---
+++ src/main/java/com/example/orchestrator/controllers/MyController.java
@@ -0,0 +1,26 @@
+package com.example.orchestrator.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.Map;
+
+@RestController
+public class MyController {
+
+    private Map<String, String> values;
+
+    @Autowired
+    public MyController() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        this.values = objectMapper.readValue("{\"key\": \"value\"}", Map.class);
+    }
+
+    @RequestMapping("/myendpoint")
+    public Map<String, String> showValues() {
+        return this.values;
+    }
+}
